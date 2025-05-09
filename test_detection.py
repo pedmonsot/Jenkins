3a1200539bdd3d406ef4f8c14d3f3e95bb9607bb
pedmonsot <pedmonsot@alu.edu.gva.es>
2025-05-09 11:41:42 +0200

    Primer commit

---
+++ test_detection.py
@@ -0,0 +1,13 @@
+import unittest
+from detector import es_maliciosa
+
+class TestDetector(unittest.TestCase):
+    def test_maliciosa(self):
+        self.assertTrue(es_maliciosa("192.168.1.10"))
+
+    def test_segura(self):
+        self.assertFalse(es_maliciosa("8.8.8.8"))
+
+if __name__ == "__main__":
+    unittest.main()
+
